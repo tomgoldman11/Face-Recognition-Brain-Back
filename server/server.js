e35d9080899e9e95a28bd216e183b35eb85fec9b
Tom Goldman <tomgoldman11@gmail.com>
2019-10-25 19:02:57 +0300

    changes for heroku

--- server/server.js
+++ server/server.js
@@ -7,10 +7,8 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'Shamen123!',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl : true,
   }
 });
 

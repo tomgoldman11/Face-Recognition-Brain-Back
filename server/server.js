d2e69535e26e7a104300e525990ef37ff8674dcc
Tom Goldman <tomgoldman11@gmail.com>
2019-10-25 16:53:23 +0300

    adding controllers

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'Shamen123!',
+    database : 'smart-brain'
+  }
+});
+
+// check if we get information from database.
+// db.select('*').from('users').then(data => {
+//   console.log(data);
+// });
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+
+app.listen(process.env.port || 3000, ()=> {
+  console.log(`app is running on port ${process.env.port}`);
+})
